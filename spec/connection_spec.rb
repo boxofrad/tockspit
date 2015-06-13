62395ceaf4094703a526fe47d6c23894c2b2375a
Daniel Upton <daniel@floppy.co>
2015-06-13 12:36:53 +0100

    Added unhappy path tests for `#clients`

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -36,16 +36,15 @@ module Tockspit
     let(:connection) { Connection.new(subscription_id, api_token) }
 
     describe "#clients" do
-      def stub_page(page, file)
+      def stub_page(page)
         stub_request(:get, "https://www.tickspot.com/#{subscription_id}/api/v2/clients.json").
-          with(headers: { "Authorization" => "Token token=#{api_token}" }, query: { page: page }).
-          to_return(body: fixture(file))
+          with(headers: { "Authorization" => "Token token=#{api_token}" }, query: { page: page })
       end
 
       example "with correct credentials" do
-        stub_page(1, "clients.json")
-        stub_page(2, "clients.json")
-        stub_page(3, "empty.json")
+        stub_page(1).to_return(body: fixture("clients.json"))
+        stub_page(2).to_return(body: fixture("clients.json"))
+        stub_page(3).to_return(body: fixture("empty.json"))
 
         clients = connection.clients
 
@@ -55,6 +54,12 @@ module Tockspit
         expect(clients[0].name).to eq "The Republic"
         expect(clients[0].updated_at).to eq DateTime.new(2014, 9, 9, 13, 36, 20)
         expect(clients[0].url).to eq "https://www.tickspot.com/api/v2/1/clients/12.json"
+      example "with incorrect credentials" do
+        stub_page(1).to_return(status: 401, body: fixture("empty.json"))
+
+        expect {
+          connection.clients.first
+        }.to raise_error BadCredentials
       end
     end
   end
